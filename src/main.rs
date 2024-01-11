d6537c9d5cd2fda3d7ff8d400f9bd9dad1f29745
Hung-Han (Henry) Chen <chenhungh@gmail.com>
2024-01-11 17:05:48 +0200

    fix: avoid quotation marks in answer, avoid duplicated instruction
    Signed-off-by: Hung-Han (Henry) Chen <chenhungh@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,19 @@ use std::error::Error;
 use std::path::PathBuf;
 
 use async_openai::types::CreateCompletionRequestArgs;
+use async_openai::{
+    types::{
+        ChatCompletionRequestSystemMessageArgs, ChatCompletionRequestUserMessageArgs,
+        CreateChatCompletionRequestArgs,
+    },
+    Client,
+};
 use clap::Parser;
 use mlx_training_rs::cli::CLI;
 use serde::Deserialize;
+use serde_json;
 use tokio::fs::{self, OpenOptions};
 use tokio::io::AsyncWriteExt;
-use serde_json;
-use async_openai::{Client, types::{CreateChatCompletionRequestArgs, ChatCompletionRequestSystemMessageArgs, ChatCompletionRequestUserMessageArgs}};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -16,7 +22,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let cli = CLI::parse();
     let topic = &cli.topic;
     let n = cli.n;
-    
+
     tokio::fs::create_dir_all("./data").await?;
     write_instruction_jsonl(topic, n).await?;
     write_train_jsonl().await?;
@@ -48,32 +54,31 @@ async fn write_instruction_jsonl(topic: &str, n: usize) -> Result<(), Box<dyn Er
             .open(&file_path)
             .await?;
     }
-    let instructions = fs::read_to_string(&file_path).await?;
-    let instructions: Vec<Instruction> = instructions.lines().map(|line| serde_json::from_str(&line).unwrap()).collect();
 
     // Open the file in append mode
-    let mut file = OpenOptions::new()
-    .append(true)
-    .open(&file_path)
-    .await?;
+    let mut file = OpenOptions::new().append(true).open(&file_path).await?;
 
     println!("------------------------------");
-    println!("{}", format!("Generating instructions on topic {}...", topic));
+    println!(
+        "{}",
+        format!("Generating instructions on topic {}...", topic)
+    );
     for _ in 0..n {
         let instruction = gen_instructions(topic).await?;
-        
-        if let Some(_) = instructions.iter().find(|i| i.text == instruction) {
+        let instructions = fs::read_to_string(&file_path).await?;
+        let instructions: Vec<Instruction> = instructions
+            .lines()
+            .map(|line| serde_json::from_str(&line).unwrap())
+            .collect();
+        if let Some(_) = instructions.iter().find(|i| i.text.contains(&instruction)) {
             // println!("Skipping duplicate instruction: {}", instruction);
             continue;
         } else {
-
             println!("------------------------------");
             println!("Writing new instruction to file: {}", instruction);
             // Write the { text: instruction } to the end of the file
-            file.write_all(format!(
-                r#"{{ "text": "{}" }}"#,
-                instruction
-            ).as_bytes()).await?;
+            file.write_all(format!(r#"{{ "text": "{}" }}"#, instruction).as_bytes())
+                .await?;
             file.write_all(b"\n").await?;
         }
     }
@@ -82,7 +87,7 @@ async fn write_instruction_jsonl(topic: &str, n: usize) -> Result<(), Box<dyn Er
 
 async fn gen_instructions(topic: &str) -> Result<String, Box<dyn Error>> {
     let client = Client::new();
-    let prompt = format!("You are generating one question on a topic {}. The one question you are generating is asked by user when using {} or exploring on topic {}. The question should start with only one of the following: Where do I, Is it okay to, Can you help me, I need to, Is there a, Do you know, Where is the, Can you tell me, Can I change, What are the, How do I, When is it, Does WordPress have, How to, What is the difference, Can users, Can I, What is. You do not need to provide an answer or category to each question. You response should only contain the generated question. Do not add quotation marks before or after the question. Only generate one question.", topic, topic, topic);
+    let prompt = format!("You are generating one question on a topic {}. The one question you are generating is asked by user when using {} or exploring on topic {}. The question should start with only one of the following: Where do I, Is it okay to, Can you help me, I need to, Is there a, Do you know, Where is the, Can you tell me, Can I change, What are the, How do I, When is it, Does WordPress have, How to, What is the difference, Can users, Can I, What is. You do not need to provide an answer or category to each question. You response should only contain the generated question. Do not add quotation marks before or after the question. Only generate one question. Pay attension to all the instructions carefully.", topic, topic, topic);
     let request = CreateCompletionRequestArgs::default()
         .max_tokens(512u16)
         .model("gpt-3.5-turbo-instruct")
@@ -90,7 +95,19 @@ async fn gen_instructions(topic: &str) -> Result<String, Box<dyn Error>> {
         .build()?;
 
     let response = client.completions().create(request).await?;
-    let result= response.choices.first().unwrap().text.clone().trim().replace('\n', "\\n").replace("\n\n", "\\n\\n").replace('\r', "\\r").replace("\"", "");
+    let result = response
+        .choices
+        .first()
+        .unwrap()
+        .text
+        .clone()
+        .trim()
+        .replace('\n', "\\n")
+        .replace("\n\n", "\\n\\n")
+        .replace('\r', "\\r")
+        // remove all " (not valid in json) from the instruction
+        .replace(r#"/""#, "")
+        .replace('"', "");
 
     Ok(result)
 }
@@ -99,7 +116,10 @@ async fn gen_instructions(topic: &str) -> Result<String, Box<dyn Error>> {
 async fn write_train_jsonl() -> Result<(), Box<dyn Error>> {
     let instructions_file_path = PathBuf::from("./data/").join("instructions.jsonl");
     let instructions = fs::read_to_string(&instructions_file_path).await?;
-    let instructions: Vec<Instruction> = instructions.lines().map(|line| serde_json::from_str(&line).unwrap()).collect();
+    let instructions: Vec<Instruction> = instructions
+        .lines()
+        .map(|line| serde_json::from_str(&line).unwrap())
+        .collect();
     let total = instructions.len();
 
     let train_file_path = PathBuf::from("./data/").join("train.jsonl");
@@ -111,10 +131,17 @@ async fn write_train_jsonl() -> Result<(), Box<dyn Error>> {
             .open(&train_file_path)
             .await?;
     }
-    let trainings: Vec<Train> = fs::read_to_string(&train_file_path).await?.lines().filter_map(|line| serde_json::from_str(&line).ok()).collect();
- 
+    let trainings: Vec<Train> = fs::read_to_string(&train_file_path)
+        .await?
+        .lines()
+        .filter_map(|line| serde_json::from_str(&line).ok())
+        .collect();
+
     for (i, instruction) in instructions.iter().enumerate() {
-        if let Some(_) = trainings.iter().find(|t| t.text.contains(&instruction.text)) {
+        if let Some(_) = trainings
+            .iter()
+            .find(|t| t.text.contains(&instruction.text))
+        {
             // println!("Skipping processing instruction {} because it can be found in train.jsonl", instruction.text);
             continue;
         } else {
@@ -161,8 +188,15 @@ async fn query_openai(user_message: &str) -> Result<String, Box<dyn Error>> {
 
 async fn process_instruction(instruction: &str) -> Result<String, Box<dyn Error>> {
     let answer = query_openai(instruction).await.unwrap();
-    println!("{}", answer);
-    let answer = answer.replace('\n', "\\n").replace("\n\n", "\\n\\n").replace('\r', "\\r").replace("\"", "/\"");
+    println!("before {}", answer);
+    let answer = answer
+        .replace('\n', "\\n")
+        .replace("\n\n", "\\n\\n")
+        .replace('\r', "\\r")
+        .replace(r#"/""#, r#"\""#)
+        .replace('"', r#"\""#);
+    println!("after {}", answer);
+
     let result = format!(
         r#"{{ "text": "<s>[INST] {}[/INST] {}</s>" }}"#,
         instruction, answer
@@ -196,8 +230,16 @@ async fn create_valid_file() -> Result<(), Box<dyn std::error::Error>> {
     let total_lines = train_lines.len();
     let twenty_percent = (total_lines as f64 * 0.2).round() as usize;
 
-    let val_lines = train_lines.iter().take(twenty_percent).cloned().collect::<Vec<&str>>();
-    let train_lines = train_lines.iter().skip(twenty_percent).cloned().collect::<Vec<&str>>();
+    let val_lines = train_lines
+        .iter()
+        .take(twenty_percent)
+        .cloned()
+        .collect::<Vec<&str>>();
+    let train_lines = train_lines
+        .iter()
+        .skip(twenty_percent)
+        .cloned()
+        .collect::<Vec<&str>>();
 
     let train = train_lines.join("\n");
     let val = val_lines.join("\n");
